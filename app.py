24c764f016c9c267b8de8c28641434f691948886
ShivaniParekh <shivaniparekh2197@gmail.com>
2026-09-11 22:18:54 +0530

    changes to deploy on render

--- app.py
+++ app.py
@@ -964,8 +964,7 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", "5000"))    
     url = f"http://127.0.0.1:{port}"
-    threading.Timer(1.0, lambda: webbrowser.open(url)).start()
-    
+
     app.run(
         host="127.0.0.1",
         port=int(os.getenv("PORT", "5000")),
